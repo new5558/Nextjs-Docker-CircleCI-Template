f163d5bdfaa1d6765d099228efbcb2b9418289df
new5558 <new17353@gmail.com>
2019-03-23 18:08:07 +0700

    test 5.3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import Map from '../components/home/Map.js'
+import Map from '../components/home/Map.js'
 import '../util/tw.css';
 
 class App extends Component {
