485f034d2e4aca7660813159db6f2d266ed77273
new5558 <new17353@gmail.com>
2019-03-23 18:17:01 +0700

    test 5.4

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import Map from '../components/home/Map.js'
 import '../util/tw.css';
 
+const env = process.env.GOOGLEMAP_API_KEY
+
 class App extends Component {
+  getInitialProps() {
+    return {"key" : env}
+  }
+
   render() {
-    for(const field in process.env) {
-      console.log(field + " : " + process.env[field]);
-    } 
-    console.log('special :'+ process.env.GOOGLEMAP_API_KEY )
-    const key = process.env.GOOGLEMAP_API_KEY;
-    console.log('key :' + key)
     return (
       <div>
-        <Map/>
+        {/* <Map/> */}
         {"test 3 : " + process.env.NODE_ENV}
-        {"?? 3 : " + process.env.GOOGLEMAP_API_KEY + "dsEs3d3HS3xP" + key}
+        {"?? 3 : " + process.env.GOOGLEMAP_API_KEY + "dsEs3d3HS3xP" + this.props.key}
       </div>
     );
   }
